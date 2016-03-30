4fc103a2ea9a3b56edfafc517d8c2923580edf0b
angie <angie-sichaca@upc.edu.co>
2016-03-30 07:14:28 -0500

    new

--- english/urls.py
+++ english/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^modulo1$', modulo1, name='modulo1' ), #/modulo1
     url(r'^unidad1$', modulo1_unidad1, name='modulo1-unidad1' ), #/unidad1
     url(r'^tema1$', unidad1_tm1, name='modulo1-unidad1-tm1' ), #/tema1
+    url(r'^tinymce/', include('tinymce.urls')),
 
     url(r'^admin/', admin.site.urls),
 ]
--- project/views.py
+++ project/views.py
@@ -10,19 +10,36 @@ def curso(request):
     return render_to_response('../templates/curso.html')
 
 def login_estudiante(request):
+    """view del login
+    """
+    #Verificamos que los datos lleguen por el methodo POST
+    formulario = LoginForm()
+    if request.method == 'POST':
+        #Cargamos el formulario (ver forms.py con los datos del POST)
+        formulario = LoginForm(data = request.POST)
+        #Verificamos que los datos esten correctos segun su estructura
+        if formulario.is_valid():
+            # Capturamos las variables que llegan por POST
+            usuario = request.POST.get('usuario')
+            clave = request.POST.get('clave')
 
-    user = request.POST.get('username')
-    clave = request.POST.get('password')
-    correcto = False
+            #validamos los datos de usuario y contraseña, el metodo authenticate verifica el password cifrado
+            acceso = auth.authenticate(username = usuario, password = clave )
+            #Si el usuario existe abrimos una sesion de usuario
 
-    if User.objects.filter(username = user, password = clave).exists():
-        correcto = True
+            if not acceso is None:
+                auth.login(request, acceso)
+                return HttpResponseRedirect('/home')
+            else:
+                formulario._errors = { NON_FIELD_ERRORS:  'Usuario o Password Invalido'}
 
-    return render_to_response('../templates/login-estudiante.html', ("correcto" , correcto), context_instance= RequestContext(request))
+
+    return render_to_response('../templates/login-estudiante.html', {"form": formulario} , context_instance = RequestContext(request))
 
 def login_profesor(request):
     return render_to_response('../templates/login-profe.html')
 
+@login_required(login_url="/login")
 def inicio_estudiante(request):
     return render_to_response('../templates/inicio-estudiante.html')
 
