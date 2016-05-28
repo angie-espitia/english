83bd2edbafae864a400bbf64e51534400f99250c
angie <angie-sichaca@upc.edu.co>
2016-05-28 18:15:08 -0500

    sabado 28

--- english/wsgi.py
+++ english/wsgi.py
@@ -34,7 +34,7 @@ application = get_wsgi_application()'''
 
 #!/usr/bin/python
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cursoshop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "english.settings")
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
